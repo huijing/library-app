43ed6630309601e04a26d21778e4176b10d56216
huijing <kakyou_tensai@yahoo.com>
2017-07-13 16:45:11 +0800

    Test minification

--- _js/scripts.js
+++ _js/scripts.js
@@ -37,39 +37,6 @@ if ('serviceWorker' in navigator) {
     }
   })(Array.prototype)
 
-  // const modBook = () => {
-  //   const bookTable = document.getElementById('jsBooks')
-  //   const editForm = document.getElementById('jsEditEntry')
-  //   const submitEdit = document.getElementById('jsSubmitEdit')
-
-  //   function update(element) {
-  //     const bookRow = element.parentElement.parentElement
-  //     editForm.elements[0].value = bookRow.id
-  //     console.log(bookRow.id)
-  //     console.log(editForm.elements[0])
-  //   }
-
-  //   bookTable.addEventListener('click', function(event) {
-  //     const action = event.target.getAttribute('data-function')
-  //     if (action === 'edit') {
-  //       update(event.target)
-  //     }
-  //   })
-
-  //   submitEdit.addEventListener('click', function(event) {
-  //     event.preventDefault()
-  //     console.log(event.target.form)
-  //     // fetch('quotes', {
-  //     //   method: 'put',
-  //     //   headers: {'Content-Type': 'application/json'},
-  //     //   body: JSON.stringify({
-  //     //     'name': 'Darth Vader',
-  //     //     'quote': 'I find your lack of faith disturbing.'
-  //     //   })
-  //     // })
-  //   })
-  // }
-
   document.addEventListener('readystatechange', function() {
     if (document.readyState === 'complete') {
       const jsListing = document.getElementById('jsListing')
@@ -77,9 +44,6 @@ if ('serviceWorker' in navigator) {
       if (jsListing) {
         tableFilter.init()
       }
-      // if (jsBooks) {
-      //   modBook()
-      // }
     }
   })
 })(document)
